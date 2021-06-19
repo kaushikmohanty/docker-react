75df6b9c6836cc2e36596b8f84f21b6d60ff16a5
Kaushik Mohanty <kaushik.mohanty@gmail.com>
2021-06-19 20:48:25 +0530

    modified app file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn react. I am the production version!
+          Learn react.
         </a>
       </header>
     </div>
